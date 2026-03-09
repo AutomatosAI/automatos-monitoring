7ae151d3eab6f5e4a4b63f6dac2ce15459c06238
Gerard Kavanagh <gerard161@gmail.com>
2026-03-09 09:26:01 +0000

    fix: move dashboards to /etc/grafana to avoid volume mount collision
    Railway volume at /var/lib/grafana wipes COPY'd dashboards.
    Move dashboards to /etc/grafana/dashboards (outside volume mount).

--- services/grafana/entrypoint.sh
+++ services/grafana/entrypoint.sh
@@ -2,7 +2,9 @@
 set -e
 
 # Fix ownership on Railway volume mount (mounted as root)
-chown -R 472:0 /var/lib/grafana /etc/grafana/provisioning
+chown -R 472:0 /var/lib/grafana
+chown -R 472:0 /etc/grafana/provisioning
+chown -R 472:0 /etc/grafana/dashboards
 
 # Run Grafana's default entrypoint as grafana user
 exec su grafana -s /bin/sh -c '/run.sh'
